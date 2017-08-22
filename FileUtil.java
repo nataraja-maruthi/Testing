24e2c3109532a1f5997f81ed4a2fbc435e3eb5df
nataraja-maruthi <nataraja.suma@gmail.com>
2017-08-22 12:12:52 +0530

    Create FileUtil.java

---
+++ FileUtil.java
@@ -0,0 +1,18 @@
+package com.test.util;
+
+public class FileUtil {
+
+	
+	public void readFile(String filePath) {
+		// code to read file 
+	}
+	
+	public void writeFile(String filePath,String fileContent) {
+		// code to read file 
+	}
+	
+	public boolean fileExist(String filePath) {
+		// code to read file
+		return true;
+	}
+}
