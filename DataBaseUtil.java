f279838cd69cd719ca3f206c2b394290403912fe
nataraja-maruthi <nataraja.suma@gmail.com>
2017-08-22 12:12:25 +0530

    Create DataBaseUtil.java

---
+++ DataBaseUtil.java
@@ -0,0 +1,16 @@
+package com.test.util;
+
+public class DataBaseUtil {
+
+	public void getDbConnection() {
+		
+	}
+	
+	public void getTableData(String tableName) {
+		// code to fetch db records
+	}
+	
+	public void deleteTableData(String tableName) {
+	    // code to delete table data	
+	}
+}
